8b8eb17969047c4daf1adff0e0180efaa21fa90d
Adrian Samaniego <llathander@gmail.com>
2019-11-18 19:04:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,9 +21,9 @@ def long_planeteer_calls(array)
 end
 
 
-def find_the_cheese(dickhead)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    dickhead.find do |x|
+    array.find do |x|
       cheese_types.include?(x)
   end
 end
